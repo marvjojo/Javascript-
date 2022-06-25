7bdb8ab415c7e9dda2be8c6ba9e3d9a2fb0cc879
Johnson Marvellous <77470833+marvjojo@users.noreply.github.com>
2022-06-25 16:35:46 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 let myName ='Johnson .O. Marvelous ';
-// console.log(myName);
+
 
 let myHeight ='6.1feet';
-// console.log(myHeight);
+
 
 let myCountry ='Nigeria';
-// console.log(myCountry);/
+
 
 document.write(myName + "<br>" + myHeight + "<br>" + myCountry)
